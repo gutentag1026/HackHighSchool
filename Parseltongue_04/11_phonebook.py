d679d44057d02bf86dadcc7c318bceaf7d6e5b73
wendy <yhuang@e1z2r9p34.42.us.org>
2018-08-02 13:47:52 -0700

    11

---
+++ Parseltongue_04/11_phonebook.py
@@ -0,0 +1,14 @@
+dic1={}
+dic2={}
+with open("name.txt") as file:
+    for line in file:
+        (firstname, lastname) = line.strip().split(",")
+        if firstname not in dic1:
+            dic1[firstname] = [lastname]
+        else:
+            dic1[firstname] += [lastname]
+print(dic1)
+
+for key, value in dic1.items():
+    print(key, value)
+    print(f"{key} ({len(value)}): {value}")
